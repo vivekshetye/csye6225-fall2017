93f167eae0fa971466888b45e505f89c2bb38353
patilmihir <patil.m@husky.neu.edu>
2017-09-27 03:58:16 -0400

    Assignment_3 Version 1.6

--- csye6225-spring-boot-starter-webapp-master/src/main/java/com/csye6225/demo/controllers/HomeController.java
+++ csye6225-spring-boot-starter-webapp-master/src/main/java/com/csye6225/demo/controllers/HomeController.java
@@ -1,52 +1,60 @@
 package com.csye6225.demo.controllers;
 
-
+import com.csye6225.demo.model.User;
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import com.google.gson.JsonObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+import com.csye6225.demo.service.UserService;
 
 import java.util.Date;
 
 @Controller
 public class HomeController {
 
+  @Autowired
+  private UserService userService;
+
   private final static Logger logger = LoggerFactory.getLogger(HomeController.class);
 
   @RequestMapping(value = "/", method = RequestMethod.GET, produces = "application/json")
   @ResponseBody
-  public String welcome() {
-
+  public String welcome(@RequestHeader(value="email", required = false) String email, @RequestHeader(value="password", required = false) String password) {
     JsonObject jsonObject = new JsonObject();
-
-    if (SecurityContextHolder.getContext().getAuthentication() != null
-        && SecurityContextHolder.getContext().getAuthentication() instanceof AnonymousAuthenticationToken) {
+    User userExists = userService.findUserByEmail(email);
+    if(userExists != null) {
+      BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+      if(encoder.matches(password, userExists.getPassword()))
+        jsonObject.addProperty("message", "you are logged in. current time is " + new Date().toString());
+      else
+        jsonObject.addProperty("message", "unauthorized");
+    }else {
       jsonObject.addProperty("message", "you are not logged in!!!");
-    } else {
-      jsonObject.addProperty("message", "you are logged in. current time is " + new Date().toString());
     }
-
     return jsonObject.toString();
+
   }
 
-  @RequestMapping(value = "/test", method = RequestMethod.GET, produces = "application/json")
+  @RequestMapping(value = "/user/register", method = RequestMethod.POST, produces = "application/json")
   @ResponseBody
-  public String test() {
+  public String register(@RequestBody User user) {
     JsonObject jsonObject = new JsonObject();
-    jsonObject.addProperty("message", "authorized for /test");
-    return jsonObject.toString();
-  }
-
-  @RequestMapping(value = "/testPost", method = RequestMethod.POST, produces = "application/json")
-  @ResponseBody
-  public String testPost() {
-    JsonObject jsonObject = new JsonObject();
-    jsonObject.addProperty("message", "authorized for /testPost");
+    User userExists = userService.findUserByEmail(user.getEmail());
+    if (userExists != null) {
+      jsonObject.addProperty("message", "Account already exist !");
+    } else {
+      User createUser = new User();
+      createUser.setEmail(user.getEmail());
+      createUser.setPassword(user.getPassword());
+      userService.saveUser(createUser);
+      jsonObject.addProperty("message", "Account created successfully");
+    }
     return jsonObject.toString();
   }
 
