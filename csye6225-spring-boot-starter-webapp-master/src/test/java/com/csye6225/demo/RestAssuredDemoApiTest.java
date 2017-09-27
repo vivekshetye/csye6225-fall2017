527b3a584ab8f923eb4126569ccc0ff5be506c41
vivekshetye <shetye.v@husky.neu.edu>
2017-09-27 17:13:39 -0400

    added one demo test

--- csye6225-spring-boot-starter-webapp-master/src/test/java/com/csye6225/demo/RestAssuredDemoApiTest.java
+++ csye6225-spring-boot-starter-webapp-master/src/test/java/com/csye6225/demo/RestAssuredDemoApiTest.java
@@ -9,6 +9,11 @@ import java.net.URISyntaxException;
 
 public class RestAssuredDemoApiTest {
 
+  @Test
+  public void defaultTest() {
+    System.out.println("---------******Demo test******--------------");
+  }
+
   @Ignore
   @Test
   public void testGetHomePage() throws URISyntaxException {
