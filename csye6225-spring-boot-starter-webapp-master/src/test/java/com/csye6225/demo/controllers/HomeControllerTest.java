7aac73f430659524d2ed05e6f45c4db2c870729c
vivekshetye <shetye.v@husky.neu.edu>
2017-09-28 02:58:11 -0400

    added new HomeControllerTest

---
+++ csye6225-spring-boot-starter-webapp-master/src/test/java/com/csye6225/demo/controllers/HomeControllerTest.java
@@ -0,0 +1,62 @@
+package com.csye6225.demo.controllers;
+
+import com.csye6225.demo.model.User;
+import com.csye6225.demo.repository.UserRepository;
+import com.csye6225.demo.service.UserService;
+import com.csye6225.demo.service.UserServiceImpl;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+
+@DataJpaTest
+@WebMvcTest(controllers = HomeController.class, secure = false)
+public class HomeControllerTest {
+
+//    @InjectMocks
+//    UserService userService;
+
+    @Mock
+    UserRepository userRepository;
+
+    @Autowired
+    MockMvc mockMvc;
+
+
+
+    @Before
+    public void setup() {
+//        this.mockMvc = MockMvcBuilders.standaloneSetup(new HomeController()).build();
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void register() throws Exception {
+        User createUser = new User();
+        createUser.setEmail("vivek");
+        createUser.setPassword("vivek");
+//        userRepository.save(createUser);
+
+        when(userRepository.findByEmail("vivek")).thenReturn(createUser);
+        User retrievedUser = userRepository.findByEmail("vivek");
+        assertEquals(retrievedUser.getEmail(),"vivek");
+    }
+
+}
