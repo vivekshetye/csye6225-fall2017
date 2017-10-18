cf336a4d87650354735d97c94c9e3c73e3b501ee
patilmihir <patil.m@husky.neu.edu>
2017-10-18 19:52:15 -0400

    updated cloudformation scripts and templates

--- csye6225-spring-boot-starter-webapp-master/infrastructure/cloudformation/create-csye6225-cloudformation-stack.sh
+++ csye6225-spring-boot-starter-webapp-master/infrastructure/cloudformation/create-csye6225-cloudformation-stack.sh
@@ -11,7 +11,9 @@
 echo "Please enter Stack Name"
 read stackName
 echo "Please enter Amazon Machine Image Id"
-read ImgId
+read imgId
+echo "Please enter the VPC ID"
+read VPCId
 echo "Please enter the instance type"
 read InstType
 echo "Please enter the volume size"
@@ -20,8 +22,6 @@ echo "Please enter the volume type"
 read volumeType
 echo "Please enter the public key name"
 read keyName
-echo "Please enter the security group name"
-read securityGroupName
 echo "Please enter the hosted Zone Id"
 read hostedZoneId
 echo "Please enter the dns name"
@@ -31,10 +31,10 @@ read recordSetType
 echo "Please enter the RecordSeT Time to Live"
 read recordSetTTL
 
-aws cloudformation create-stack --stack-name $stackName --enable-termination-protection --template-body file:///home/patilmihir/Assignment_4/PART_2/Demo.json --parameters ParameterKey=ImgId,ParameterValue=$ImgId ParameterKey=InstType,ParameterValue=$InstType ParameterKey=volumeSize,ParameterValue=$volumeSize ParameterKey=volumeType,ParameterValue=$volumeType ParameterKey=keyName,ParameterValue=$keyName ParameterKey=securityGroupName,ParameterValue=$securityGroupName ParameterKey=hostedZoneId,ParameterValue=$hostedZoneId ParameterKey=dnsName,ParameterValue=$dnsName ParameterKey=recordSetType,ParameterValue=$recordSetType ParameterKey=recordSetTTL,ParameterValue=$recordSetTTL
+aws cloudformation create-stack --stack-name $stackName --enable-termination-protection --template-body file:///home/patilmihir/Assignment_4/PART_2/Demo.json --parameters ParameterKey=ImgId,ParameterValue=$ImgId ParameterKey=VPCId,ParameterValue=$VPCId ParameterKey=InstType,ParameterValue=$InstType ParameterKey=volumeSize,ParameterValue=$volumeSize ParameterKey=volumeType,ParameterValue=$volumeType ParameterKey=keyName,ParameterValue=$keyName ParameterKey=hostedZoneId,ParameterValue=$hostedZoneId ParameterKey=dnsName,ParameterValue=$dnsName ParameterKey=recordSetType,ParameterValue=$recordSetType ParameterKey=recordSetTTL,ParameterValue=$recordSetTTL
 
 
-
+#aws cloudformation create-stack --stack-name teststack --template-body file:///home/patilmihir/Assignment_3/csye6225-fall2017/csye6225-spring-boot-starter-webapp-master/infrastructure/cloudformation/stack-template.json --parameters ParameterKey=imgId,ParameterValue=ami-cd0f5cb6 ParameterKey=vpcId,ParameterValue=vpc-2433d45c ParameterKey=keyName,ParameterValue=id_rsa ParameterKey=InstType,ParameterValue=t2.micro ParameterKey=volumeSize,ParameterValue=16 ParameterKey=volumeType,ParameterValue=gp2 ParameterKey=hostedZoneId,ParameterValue=Z31TTYNFTW06J1 ParameterKey=dnsName,ParameterValue=ec2.csye6225-fall2017-patilm.me ParameterKey=recordSetType,ParameterValue=A ParameterKey=recordSetTTL,ParameterValue=60
 
 
 
