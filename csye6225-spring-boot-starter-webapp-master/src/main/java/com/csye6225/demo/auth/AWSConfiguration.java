ca4f953c3b5b82080c2af9e7a1112c2b4c4332d0
vivekshetye <shetye.v@husky.neu.edu>
2017-11-08 13:48:28 -0500

    version 8.2

--- csye6225-spring-boot-starter-webapp-master/src/main/java/com/csye6225/demo/auth/AWSConfiguration.java
+++ csye6225-spring-boot-starter-webapp-master/src/main/java/com/csye6225/demo/auth/AWSConfiguration.java
@@ -6,7 +6,9 @@ import com.amazonaws.auth.InstanceProfileCredentialsProvider;
 import com.amazonaws.auth.profile.ProfileCredentialsProvider;
 import com.amazonaws.regions.Region;
 import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,14 +21,14 @@ public class AWSConfiguration {
 
 
 
-    @Bean
-    public BasicAWSCredentials basicAWSCredentials() {
-        return new BasicAWSCredentials("AKIAJP2M6S2UBJZFDV2A", "ZpuFiCcbVQY2mWHJK2BT3usOUriMYJQ9x/gMN7ea");
-    }
+//    @Bean
+//    public BasicAWSCredentials basicAWSCredentials() {
+//        return new BasicAWSCredentials("AKIAJP2M6S2UBJZFDV2A", "ZpuFiCcbVQY2mWHJK2BT3usOUriMYJQ9x/gMN7ea");
+//    }
 
     @Bean
-    public AmazonS3Client amazonS3Client() {
-        AmazonS3Client amazonS3Client = new AmazonS3Client(new InstanceProfileCredentialsProvider(false));
+    public AmazonS3 amazonS3Client() {
+        AmazonS3 amazonS3Client = AmazonS3ClientBuilder.standard().withCredentials(new InstanceProfileCredentialsProvider(false)).build();
         amazonS3Client.setRegion(Region.getRegion(Regions.fromName(region)));
         return amazonS3Client;
 //        AmazonS3 s3Client=new AmazonS3Client();
